c4bf9a6bd4ef22e6569e498bf51cde9ccbbb3ff2
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-07-16 17:22:36 +0100

    test: add tests for effect display

--- test/unit/test_effect.gd
+++ test/unit/test_effect.gd
@@ -13,19 +13,24 @@ func after_all():
 func test_add():
 	var effect = effect_zone.Effect.new(effect_type.ADD, 2)
 	assert_eq(effect.apply(5), 7)
+	assert_eq(effect.get_display(), "+2")
 
 func test_substract():
 	var effect = effect_zone.Effect.new(effect_type.SUBSTRACT, 2)
 	assert_eq(effect.apply(5), 3)
+	assert_eq(effect.get_display(), "-2")
 
 func test_substract_below_zero():
 	var effect = effect_zone.Effect.new(effect_type.SUBSTRACT, 7)
 	assert_eq(effect.apply(5), 0)
+	assert_eq(effect.get_display(), "-7")
 
 func test_multiply():
 	var effect = effect_zone.Effect.new(effect_type.MULTIPLY, 2)
 	assert_eq(effect.apply(5), 10)
+	assert_eq(effect.get_display(), "x2")
 
 func test_divide():
 	var effect = effect_zone.Effect.new(effect_type.DIVIDE, 2)
 	assert_eq(effect.apply(5), 2)
+	assert_eq(effect.get_display(), "/2")
